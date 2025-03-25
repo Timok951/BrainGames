93d43b296abae1343de5132ad4e78562565a04f2
Timofey <53184287+Timok951@users.noreply.github.com>
2025-03-25 22:12:51 +0300

    connections

--- Assets/Project/Scripts/GameplayManager.cs
+++ Assets/Project/Scripts/GameplayManager.cs
@@ -1,4 +1,4 @@
-using Connect.Common;
+﻿using Connect.Common;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -22,7 +22,7 @@ namespace Connect.Core {
         [SerializeField] private SpriteRenderer _clickHighlight;
         [SerializeField] private float _cellSize = 1f;
         [SerializeField] private float _cellSpacing = 0.1f;
-        [SerializeField] private float _boardPadding = 0.08f; 
+        //Initializing
         private void Awake()
         {
             Instance = this;
@@ -32,7 +32,9 @@ namespace Connect.Core {
             _titleText.gameObject.SetActive(true);
             _titleText.text = "Level " + GameManager.Instance.CurrentLevel.ToString();
 
+            CurrentLevelData = GameManager.Instance.GetLevel();
             SpawndBoard();
+            SpawnNodes();
         }
         #endregion
 
@@ -42,7 +44,7 @@ namespace Connect.Core {
         [SerializeField] private SpriteRenderer _boardPrefab, _bgcellPrefab;
         private void SpawndBoard()
         {
-            int currentLevelSize = Mathf.Min(GameManager.Instance.CurrentLevel + 2, 14);
+            int currentLevelSize = Mathf.Min(GameManager.Instance.CurrentLevel + 1, 14);
             float totalCellSize = _cellSize + _cellSpacing;
             float boardSize = currentLevelSize * totalCellSize;
             float boardScaleFactor = 1.1f;
@@ -84,24 +86,176 @@ namespace Connect.Core {
         private LevelData CurrentLevelData;
         [SerializeField] private Node _nodePrefab;
         private List<Node> _nodes;
-        public Dictionary<Vector2Int, Node> _nodeGridsS;
+        public Dictionary<Vector2Int, Node> _nodeGrid;
 
 
         public Dictionary<Vector2Int, Node> Nodes;
-
+        //Spawning nodes
         private void SpawnNodes()
         {
+            _nodes = new List<Node>();
+            _nodeGrid = new Dictionary<Vector2Int, Node>();
+            int currentLevelSize = Mathf.Min(GameManager.Instance.CurrentLevel + 2, 14);
+            float totalCellSize = _cellSize + _cellSpacing;
+            Node spawnedNode;
+            Vector3 spawnPos;
 
+            //Spawning nodes for evry cell
+            for (int i = 0; i < currentLevelSize; i++)
+            {
+                for (int j = 0; j < currentLevelSize; j++)
+                {
+                    spawnPos = new Vector3(i * totalCellSize + totalCellSize / 2f, j * totalCellSize + totalCellSize / 2f, 0f);
+                    spawnedNode = Instantiate(_nodePrefab, spawnPos, Quaternion.identity);
+                    spawnedNode.transform.localScale = Vector3.one * _cellSize;
+
+                    spawnedNode.Init();
+                    int colorIdForSpawnNode = GetColorId(i, j);
+
+                    if (colorIdForSpawnNode != -1)
+                    {
+                        spawnedNode.SetColorForPoint(colorIdForSpawnNode);
+                    }
+                    Vector2Int pos = new Vector2Int(i, j);
+                    if (_nodeGrid.ContainsKey(pos))
+                    {
+                        Debug.LogError($"Duplicate Node at {pos}");
+                    }
+                    _nodes.Add(spawnedNode);
+                    _nodeGrid.Add(new Vector2Int(i, j), spawnedNode);
+                    spawnedNode.gameObject.name = i.ToString() + j.ToString();
+                    spawnedNode.Pos2D = new Vector2Int(i, j);
+                }
+            }
+            List<Vector2Int> offsetPos = new List<Vector2Int>()
+            { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+            foreach (var item in _nodeGrid)
+            {
+                foreach (var offset in offsetPos)
+                {
+                    var checkPos = item.Key + offset;
+                    if (_nodeGrid.ContainsKey(checkPos))
+                    {
+                        item.Value.SetEdge(offset, _nodeGrid[checkPos]);
+                    }
+                }
+            }
         }
+        //Getting color for hilghlight
+        public Color GetHighLightColor(int colorId)
+        {
+            Color result = NodeColors[colorId];
+            result.a = 0.4f;
+            return result;
+        }
+
+
 
         public List<Color> NodeColors;
 
+        //Getting color ID
+        public int GetColorId(int i, int j)
+        {
+            List<Edge> edges = CurrentLevelData.Edges;
+            Vector2Int point = new Vector2Int(i, j);
+            for (int colorId = 0; colorId < edges.Count; colorId++)
+            {
+                if (edges[colorId].StartPoint == point || edges[colorId].EndPoint == point)
+                {
+                    return colorId;
+                }
+            }
+            Debug.Log($"Checking color for Node ({i},{j}), Total Edges: {edges.Count}");
+            return -1;
+        }
+
         #endregion
         #region Update_Methods
+        //Drawingedges
+        private Node startNode;
+
+        private void Update()
+        {
+            if (hasGameFinished) return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                startNode = null;
+                return;
+            }
+            if (Input.GetMouseButton(0))
+            {   //Drawing connect with color
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+                if (startNode == null)
+                {
+                    if (hit && hit.collider.gameObject.TryGetComponent(out Node tNode) && tNode.IsClickable)
+                    {
+                        startNode = tNode;
+                        _clickHighlight.gameObject.SetActive(true);
+                        _clickHighlight.gameObject.transform.position = (Vector3)mousePos2D;
+                        _clickHighlight.color = GetHighLightColor(tNode.colorId);
+                    }
+
+                    return;
+                }
+
+                _clickHighlight.gameObject.transform.position = (Vector3)mousePos2D;
+
+                if (hit && hit.collider.gameObject.TryGetComponent(out Node tmpNode) && startNode != tmpNode)
+                {
+                    if (startNode.colorId != tmpNode.colorId && tmpNode.IsEndNode)
+                    {
+                        return;
+                    }
+                    startNode.UpdateInput(tmpNode);
+                    CheckWin();
+                    startNode = null;
+                }
+                return;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                //Highlight 
+
+                startNode = null;
+                _clickHighlight.gameObject.SetActive(false);
+
+
+            }
+
+        }
         #endregion
 
         #region WIN_CONDITION 
+        //Win condition
+        private void CheckWin()
+        {
+            bool IsWinning = true;
 
+            foreach (var item in _nodes)
+            {
+                item.SolveHighlight();
+            }
+            //if evry node in win position
+            foreach (var item in _nodes)
+            { //IsWinning = IsWinning && item.Iswin;
+                IsWinning &= item.Iswin;
+                if (!IsWinning)
+                {
+                    return;
+                }
+
+            }
+            //Unlock new level and hilight the text 
+            GameManager.Instance.UnlockLevel();
+            _winText.gameObject.SetActive(true);
+            _clickHighlight.gameObject.SetActive(false);
+
+            hasGameFinished = true;
+        }
         #endregion
 
         #region BUTTON_FUNCTIONS
--- Assets/Project/Scripts/Node.cs
+++ Assets/Project/Scripts/Node.cs
@@ -1,18 +1,151 @@
+using Connect.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+namespace Connect.Common
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Node : MonoBehaviour
     {
-        
-    }
+        #region Node_Variables
+        [SerializeField] private GameObject _point;
+        [SerializeField] private GameObject _topEdge;
+        [SerializeField] private GameObject _bottomEdge;
+        [SerializeField] private GameObject _leftEdge;
+        [SerializeField] private GameObject _rightEdge;
+        [SerializeField] private GameObject _highlight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [HideInInspector] private Dictionary<Node, GameObject> ConnectEdges;
+
+        [HideInInspector] public int colorId;
+
+        public Vector2Int Pos2D { get; set; }
+        #endregion
+
+        #region DYNAMIC_VARIABLES
+        //if node connected to another node ConnectNodes.Count = 2 than IsWin = true
+        public bool IsWin
+        {
+            get
+            {
+                if (_point.activeSelf)
+                {
+                    return ConnectedNodes.Count == 1;
+                }
+                return ConnectedNodes.Count == 2;
+            }
+        }
+
+        //If active-clickable if not-not clickable
+        public bool IsClickable
+        {
+            get
+            {
+                if (_point.activeSelf)
+                {
+                    return true;
+                }
+                //If thero we cannot edit
+                return ConnectedNodes.Count > 0;
+            }
+        }
+        //Expression-bodied member(getter) if activeself == true if not false
+        public bool IsEndNode => _point.activeSelf;
+
+        public bool Iswin { get; internal set; }
+        #endregion
+
+        #region StartingMethods
+        //Disable edges(display only node) 
+        internal void Init()
+        {
+            _point.SetActive(false);
+            _topEdge.SetActive(false);
+            _bottomEdge.SetActive(false);
+            _leftEdge.SetActive(false);
+            _highlight.SetActive(false);
+
+            _rightEdge.SetActive(false);
+
+            ConnectEdges = new Dictionary<Node, GameObject>();
+        }
+
+        //Display color for nodes in level
+        public void SetColorForPoint(int colorIdForSpawnNode)
+        {
+            colorId = colorIdForSpawnNode;
+            _point.SetActive(true);
+            Color color = GameplayManager.Instance.NodeColors[colorId];
+            Debug.Log($"Setting color for node {gameObject.name}: {color}, Alpha: {color.a}");
+
+            _point.GetComponent<SpriteRenderer>().color = GameplayManager.Instance.NodeColors[colorId];
+
+        }
+
+        //Seting edgedges for extending them
+        public void SetEdge(Vector2Int offset, Node node)
+        {
+
+            if (offset == Vector2Int.up)
+            {
+                ConnectEdges[node] = _topEdge;
+                return;
+            }
+            if (offset == Vector2Int.down)
+            {
+                ConnectEdges[node] = _bottomEdge;
+                return;
+            }
+            if (offset == Vector2Int.right)
+            {
+                ConnectEdges[node] = _rightEdge;
+                return;
+            }
+            if (offset == Vector2Int.left)
+            {
+                ConnectEdges[node] = _leftEdge;
+                return;
+            }
+
+
+        }
+        #endregion
+
+        #region UPDATE_METHODS
+
+        public void UpdateInput(Node connectedNode)
+        {
+            if (!ConnectEdges.ContainsKey(connectedNode))
+            {
+                return;
+            }
+            AddEdge(connectedNode);
+        }
+        [HideInInspector] public List<Node> ConnectedNodes;
+
+
+        //Adding edge for node 
+        private void AddEdge(Node connectedNode)
+        {
+
+            connectedNode.colorId = colorId;
+            connectedNode.ConnectedNodes.Add(this);
+            ConnectedNodes.Add(connectedNode);
+            GameObject connectedEdge = ConnectEdges[connectedNode];
+            connectedEdge.SetActive(true);
+            connectedEdge.GetComponent<SpriteRenderer>().color = GameplayManager.Instance.NodeColors[colorId];
+            Debug.Log($"Added edge: {this.name} <--> {connectedNode.name}, Total connections: {ConnectedNodes.Count}");
+        }
+
+
+        //Highlighting solving Node
+        internal void SolveHighlight()
+        {
+
+
+        }
+
+        #endregion
     }
 }
